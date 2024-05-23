958862b16c78da9a4e1775b8aa59c29b7a39f8aa
nitzanto <nitzan16001@gmail.com>
2024-05-23 23:58:50 +0300

    chore: added validation

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   const isDatabaseConnected = await testDatabaseConnection(app.get(Connection));
+
   app.enableCors({
     origin: 'localhost:3000',
   });
@@ -14,6 +15,12 @@ async function bootstrap() {
     ? logger.log(isDatabaseConnected)
     : logger.error(isDatabaseConnected);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
