ee38d571c763b5bb5f76a238ba030adc3344c0ae
nitzanto <nitzan16001@gmail.com>
2024-05-23 23:58:19 +0300

    chore: added cors

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { DataSource as Connection } from 'typeorm';
 import { testDatabaseConnection } from '../libs/common/src';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   const isDatabaseConnected = await testDatabaseConnection(app.get(Connection));
+  app.enableCors({
+    origin: 'localhost:3000',
+  });
   isDatabaseConnected
     ? logger.log(isDatabaseConnected)
     : logger.error(isDatabaseConnected);
+
   await app.listen(3000);
 }
 bootstrap();
