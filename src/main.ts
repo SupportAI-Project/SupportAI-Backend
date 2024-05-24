14bfad67092d5bd0b9c057b5ddae55f9af4f39ab
nitzanto <nitzan16001@gmail.com>
2024-05-24 14:08:41 +0300

    fix: deleted util function for db

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { DataSource as Connection } from 'typeorm';
-import { testDatabaseConnection, config } from '@app/common';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { config } from '@app/common';
+import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
-  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  const isDatabaseConnected = await testDatabaseConnection(app.get(Connection));
 
   app.enableCors({
     origin: config.FRONTEND_URL,
   });
-
-  isDatabaseConnected
-    ? logger.log(isDatabaseConnected)
-    : logger.error(isDatabaseConnected);
 
   app.useGlobalPipes(
     new ValidationPipe({
