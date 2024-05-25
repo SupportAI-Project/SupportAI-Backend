83fffccb083848c5dc9e6b360ab76ab6f072c722
JosephOri <ori100no@gmail.com>
2024-05-25 12:55:56 +0300

    install zod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from '@app/common';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(databaseConfig()),
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
