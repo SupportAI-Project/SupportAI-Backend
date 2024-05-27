bb72d17f41f6a69b6203f345c28fa1795bdcbdab
Ori <ori100no@gmail.com>
2024-05-27 19:31:12 +0300

    login and register are now working

--- src/auth/user/user.service.ts
+++ src/auth/user/user.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './model/user.model';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
-import { SALT_ROUNDS } from 'src/constants/constants';
+import { ERROR_MESSAGES, SALT_ROUNDS } from 'src/constants/constants';
 
 @Injectable()
 export class UserService {
@@ -28,18 +28,19 @@ export class UserService {
       const newUser = this.userRepository.create(createUserDto);
       if (!newUser) {
         console.log('error creating user');
+        throw new InternalServerErrorException(ERROR_MESSAGES.CREATE_USER);
       }
       await this.userRepository.save(newUser);
       return newUser;
     } catch (error) {
       console.log('error creating user: ', error);
+      throw new InternalServerErrorException(ERROR_MESSAGES.CREATE_USER);
     }
   }
 
   async getUserByUsername(username: string) {
     try {
       const user = await this.userRepository.findOne({ where: { username } });
-      console.log('user: ', user);
       return user;
     } catch (error) {
       console.log('error getting user by username: ', error);
