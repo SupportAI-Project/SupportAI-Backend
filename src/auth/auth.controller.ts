ba768fe3c22d42a0aefd1530a95aee2468251f6b
Ori <ori100no@gmail.com>
2024-07-01 15:43:35 +0300

    added cookie parser

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,26 +7,34 @@ import {
   Post,
   Res,
   UseGuards,
+  Get,
+  Request,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { LoginDto } from './dto/login.dto';
-import { AuthGuard } from './guards/auth.guard';
+import { MyAuthGuard } from './guards/auth.guard';
 import { AuthService } from './auth.service';
 import { User } from './user/entity/user.model';
 import { CreateUserDto } from './user/dto/create-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { TWO_HOURS_FROM_NOW_DATE } from '@app/common/constants/auth/auth.constants';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(AuthGuard('local'))
   @HttpCode(HttpStatus.OK)
   @Post('login')
   async login(
     @Body() loginDto: LoginDto,
     @Res({ passthrough: true }) response: Response,
   ) {
-    const jwtToken = await this.authService.login(loginDto, response);
-    response.send(jwtToken);
+    const jwtToken = await this.authService.login(loginDto);
+    response.cookie('access_token', jwtToken, {
+      httpOnly: true,
+      expires: TWO_HOURS_FROM_NOW_DATE,
+    });
   }
 
   @HttpCode(HttpStatus.CREATED)
@@ -34,11 +42,17 @@ export class AuthController {
   register(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.authService.register(createUserDto);
   }
-  @UseGuards(AuthGuard)
+  @UseGuards(MyAuthGuard)
   @HttpCode(HttpStatus.OK)
   @Post('logout')
   async logout(@Res({ passthrough: true }) response: Response) {
     Logger.log('User logged out', 'AuthController');
     await this.authService.logout(response);
+  }
+
+  @UseGuards(MyAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
