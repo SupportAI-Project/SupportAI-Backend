5d0fa82c292df85423bf68b5e1ee6c6d79a4b6ff
Ori <ori100no@gmail.com>
2024-07-02 23:54:49 +0300

    removed unedded comment and file

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,4 +1,3 @@
-// src/auth/roles.guard.ts
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from '../roles/role.enum';
