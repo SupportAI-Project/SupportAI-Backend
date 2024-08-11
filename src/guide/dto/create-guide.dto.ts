e7a8ffbf27ba15c2a78674beae1fa06ee56f9ada
Ori <ori100no@gmail.com>
2024-08-11 14:27:21 +0300

    created create guide dto

--- src/guide/dto/create-guide.dto.ts
+++ src/guide/dto/create-guide.dto.ts
@@ -1,1 +1,12 @@
-export class CreateGuideDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateGuideDto {
+  @IsNumber()
+  creatorId: number;
+
+  @IsString()
+  title: string;
+
+  @IsString()
+  contentHTML: string;
+}
