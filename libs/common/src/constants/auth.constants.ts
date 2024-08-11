dde8a7108f4f9995d9ec62354ff7d19c945beba8
Ori <ori100no@gmail.com>
2024-08-11 15:17:14 +0300

    fixed runtime issue

--- libs/common/src/constants/auth.constants.ts
+++ libs/common/src/constants/auth.constants.ts
@@ -1,11 +1,9 @@
-import ms from 'ms';
-
 export const SALT_ROUNDS = 13;
 
 export const TWO_HOURS_FROM_NOW_DATE = new Date(
   Date.now() + 2 * 60 * 60 * 1000,
 );
 
-export const THREE_DAYS = ms('3d');
+export const THREE_DAYS = 3 * 24 * 60 * 60 * 1000;
 
 export const JWT_EXPIRATION_TIME = '2h';
