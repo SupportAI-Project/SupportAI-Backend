b532df75a87d01b7b14d99d8f26a19fcee44edc6
Ori <ori100no@gmail.com>
2024-08-11 12:49:46 +0300

    changed expiration

--- libs/common/src/constants/auth.constants.ts
+++ libs/common/src/constants/auth.constants.ts
@@ -1,7 +1,11 @@
+import ms from 'ms';
+
 export const SALT_ROUNDS = 13;
 
 export const TWO_HOURS_FROM_NOW_DATE = new Date(
   Date.now() + 2 * 60 * 60 * 1000,
 );
+
+export const THREE_DAYS = ms('3d');
 
 export const JWT_EXPIRATION_TIME = '2h';
