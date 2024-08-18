2fb4318197c771c6b431d5689ce310cf749e4572
Ori <ori100no@gmail.com>
2024-08-18 16:27:09 +0300

    added swagger to dto

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,5 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateReviewDto {
+  @ApiProperty({
+    description: 'ID of the guide being reviewed',
+    example: 1,
+  })
   guideId: number;
+
+  @ApiProperty({
+    description: 'Rating given by the user (1-5 stars)',
+    example: 5,
+  })
   stars: number;
+
+  @ApiProperty({
+    description: 'Optional comment provided by the user',
+    example: 'This guide was very helpful!',
+    required: false,
+  })
   comment?: string;
 }
