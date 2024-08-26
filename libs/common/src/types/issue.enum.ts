37a0af8c828fbfe007cec99637ffe756b934a0a2
Tomer <tomermi123@gmail.com>
2024-08-26 18:18:00 +0300

    feat: Add Issue enum to be used in Guides.

---
+++ libs/common/src/types/issue.enum.ts
@@ -0,0 +1,13 @@
+enum Issue {
+    ACCOUNT_ACCESS_ISSUES = 'Account Access Issues',
+    PAYMENT_PROCESSING_ERRORS = 'Payment Processing Errors',
+    SYSTEM_PERFORMANCE_OR_SLOWDOWN = 'System Performance or Slowdown',
+    LOGIN_OR_AUTHENTICATION_FAILURES = 'Login or Authentication Failures',
+    DATA_SYNCING_PROBLEMS = 'Data Syncing Problems',
+    SOFTWARE_INSTALLATION_TROUBLES = 'Software Installation Troubles',
+    HARDWARE_ISSUES = 'Hardware Issues',
+    CONNECTIVITY_OR_NETWORK_ISSUES = 'Connectivity or Network Issues',
+    PERMISSION_OR_ROLE_ISSUES = 'Permission or Role Issues',
+    UNEXPECTED_SYSTEM_CRASHES = 'Unexpected System Crashes',
+    ERROR_MESSAGES_OR_CODE_FAILURES = 'Error Messages or Code Failures'
+}
