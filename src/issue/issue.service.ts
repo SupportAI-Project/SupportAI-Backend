becdffda48cfc3caecd3bca4109cf7329d3fca92
Tomer <tomermi123@gmail.com>
2024-08-27 15:56:30 +0300

    refactor: Remove try\catch, Simplify error handling in IssueService

--- src/issue/issue.service.ts
+++ src/issue/issue.service.ts
@@ -13,64 +13,38 @@ export class IssueService {
   ) {}
 
   async createIssue(createIssueDto: CreateIssueDto): Promise<Issue> {
-    try{
-      const newIssue = this.issueRepository.create({
-        ...createIssueDto,
-      });
-      return await this.issueRepository.save(newIssue);
-
-    } catch (error) {
-      Logger.error(error);
-      throw new InternalServerErrorException("Error creating issue");
-    }
+    const newIssue = this.issueRepository.create({
+      ...createIssueDto,
+    });
+    return await this.issueRepository.save(newIssue);
   };
 
   async getAllIssues(): Promise<Issue[]> {
-    try{
-      return await this.issueRepository.find();
-    } catch (error) {
-      Logger.error(error);
-      throw new InternalServerErrorException("Error retrieving issues");
-    }
+    return await this.issueRepository.find();
   };
 
   async getIssue(id: number): Promise<Issue> {
-    try{
-      const issue = await this.issueRepository.findOne({ where: { id } });
-      if(!issue) {
-        throw new NotFoundException(`Issue with ID ${id} not found`);
-      }
-      return issue;
-    } catch (error) {
-      Logger.error(error);
-      throw new InternalServerErrorException("Error retrieving issue");
+    const issue = await this.issueRepository.findOne({ where: { id } });
+    if(!issue) {
+      Logger.error(`Issue with ID ${id} not found`);
     }
+    return issue;
   };
 
   async updateIssue(id: number, updateIssueDto: UpdateIssueDto): Promise<Issue> {
-    try{
-      await this.issueRepository.update(id, updateIssueDto);
-      const updatedIssue = await this.issueRepository.findOne({ where: { id } });
-      if (!updatedIssue) {
-        throw new NotFoundException(`Issue with ID ${id} not found`);
-      };
-      return updatedIssue;
-    } catch (error){
-      Logger.error(error);
-      throw new InternalServerErrorException("Error updating issue");
-    }
+    await this.issueRepository.update(id, updateIssueDto);
+    const updatedIssue = await this.issueRepository.findOne({ where: { id } });
+    if (!updatedIssue) {
+      Logger.error(`Issue with ID ${id} not found`);
+    };
+    return updatedIssue;
   }
 
   async deleteIssue(id: number): Promise<boolean> {
-    try{
-      const result = await this.issueRepository.delete(id);
-      if(result.affected === 0){
-        return false;
-      }
-      return true;
-    } catch (error) {
-      Logger.error(error);
-      throw new InternalServerErrorException("Error deleting issue");
+    const result = await this.issueRepository.delete(id);
+    if(result.affected === 0){
+      return false;
     }
+    return true;
   }
 }
