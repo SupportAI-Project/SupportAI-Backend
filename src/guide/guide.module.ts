59fadc4e0f49b64cdef86841cc90f55f952c7073
Ori <ori100no@gmail.com>
2024-08-27 17:03:15 +0300

    feat: Add Issue entity to Guide module imports, when creating a guide check if the issue exists and if not create it

--- src/guide/guide.module.ts
+++ src/guide/guide.module.ts
@@ -3,9 +3,10 @@ import { GuideService } from './guide.service';
 import { GuideController } from './guide.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Guide } from './entities/guide.entity';
+import { Issue } from 'src/issue/entities/issue.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Guide])],
+  imports: [TypeOrmModule.forFeature([Guide, Issue])],
   controllers: [GuideController],
   providers: [GuideService],
 })
